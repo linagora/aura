ccb66095900e3ef47cf497bd306003cd4b944f78
Michael Bailly <mbailly@linagora.com>
2012-10-18 15:45:38 +0200

    use calendar routes in express app

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
-
+var calendarRoutes = require("./routes/calendar");
 
 // app.use(express.directory('src'));
+
+calendarRoutes(app);
+
 app.use(express.static('src'))
 
 app.listen(3000);
