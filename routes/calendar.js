698eefbecc1a07399d9be4fe3497c7422f7d0f79
Michael Bailly <mbailly@linagora.com>
2012-10-18 15:45:03 +0200

    better boilerplate

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,5 +1,13 @@
 exports = module.exports = function(app) {
-  app.get('/', function(req, res){
-    res.send('hello world');
+  app.get("/calendar/event/:id", function(req, res){
+    res.send("Sending calendar event "+req.params.id);
+  });
+  
+  app.put("/calendar/event/:id", function(req, res){
+    res.send("Storing calendar event "+req.params.id);
+  });
+  
+  app.del("/calendar/event/:id", function(req, res){
+    res.send("deleting calendar event "+req.params.id);
   });
 };
