374070a9978e5646e082d44351a7b42d1d419ef8
Michel-Marie MAUDET <michel.maudet@linagora.com>
2012-10-19 06:44:52 +0200

    add offline HTML5 support

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/cache.manifest', function(req, res){
+  res.set('Content-Type', 'text/html');
+  res.sendFile('/cache.manifest');
+});
 
 // app.use(express.directory('src'));
 app.use(express.static('src'))
